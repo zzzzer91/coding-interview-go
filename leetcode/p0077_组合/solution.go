aea3ae98ea31807af1eff3a6d6bc2debff66e121
zzzzer <zzzzer91@gmail.com>
2020-09-08 15:40:43 +0800

    feat: update

---
+++ leetcode/p0077_组合/solution.go
@@ -0,0 +1,22 @@
+package p0077_组合
+
+func combine(n int, k int) [][]int {
+	a := make([]int, 0, k)
+	var res [][]int
+	var f func(u int)
+	f = func(u int) {
+		if len(a) == k {
+			temp := make([]int, k)
+			copy(temp, a)
+			res = append(res, temp)
+			return
+		}
+		for i := u + 1; i <= n; i++ {
+			a = append(a, i)
+			f(i)
+			a = a[:len(a)-1]
+		}
+	}
+	f(0)
+	return res
+}
