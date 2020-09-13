bd966b3aae4591994f9c702d1b1936cb32696631
zzzzer <zzzzer91@gmail.com>
2020-09-13 19:58:25 +0800

    feat: update

--- basis/other/p006_多人运动/main.go
+++ basis/other/p006_多人运动/main.go
@@ -2,5 +2,63 @@
 
 package main
 
+import (
+	"container/heap"
+	"fmt"
+	"sort"
+)
+
+type SortData [][]int
+
+func (d SortData) Len() int {
+	return len(d)
+}
+
+func (d SortData) Less(i, j int) bool {
+	return d[i][0] < d[j][0]
+}
+
+func (d SortData) Swap(i, j int) {
+	d[i], d[j] = d[j], d[j]
+}
+
+type MinHeap SortData
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[j]
+}
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.([]int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	tmp := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return tmp
+}
+
 func main() {
+	fmt.Println(np([][]int{{0, 30}, {5, 10}, {15, 20}})) // 2
+}
+
+func np(intervals [][]int) int {
+	sort.Sort(SortData(intervals))
+	var h MinHeap
+	heap.Push(&h, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if h[0][1] < intervals[i][0] {
+			heap.Pop(&h)
+		}
+		heap.Push(&h, intervals[i])
+	}
+	return h.Len()
 }
