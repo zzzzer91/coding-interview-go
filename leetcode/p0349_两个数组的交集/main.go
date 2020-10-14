a11797f6494758c0745748c3eeaac871daab77b6
zzzzer <zzzzer91@gmail.com>
2020-10-14 08:46:40 +0800

    feat: update

--- leetcode/p0349_两个数组的交集/main.go
+++ leetcode/p0349_两个数组的交集/main.go
@@ -10,12 +10,12 @@ func intersection(nums1 []int, nums2 []int) []int {
 	var res []int
 	m := make(map[int]int, len(nums1))
 	for _, num := range nums1 {
-		m[num] = 0
+		m[num] = 1
 	}
 	for _, num := range nums2 {
-		if v, ok := m[num]; ok && v == 0 {
+		if m[num] == 1 {
 			res = append(res, num)
-			m[num]++
+			m[num] = 0
 		}
 	}
 	return res
