a9fc0cf86df3f8fbfb12fba1fbf14298acba361e
zzzzer <zzzzer91@gmail.com>
2020-10-17 10:18:04 +0800

    feat: update

---
+++ leetcode/p0051_N皇后/main.go
@@ -0,0 +1,39 @@
+// https://leetcode-cn.com/problems/n-queens/
+
+package main
+
+func main() {
+
+}
+
+func solveNQueens(n int) [][]string {
+	var res [][]string
+	colSt, digSt, uDigSt := make([]bool, n), make([]bool, n*2), make([]bool, n*2)
+	comb := make([]byte, n*n)
+	for i := range comb {
+		comb[i] = '.'
+	}
+	var dfs func(u int)
+	dfs = func(u int) {
+		if u == n {
+			temp := make([]string, n)
+			for i := range temp {
+				temp[i] = string(comb[i*n : (i+1)*n])
+			}
+			res = append(res, temp)
+			return
+		}
+		for i := 0; i < n; i++ {
+			if colSt[i] || digSt[i+u] || uDigSt[i-u+n] {
+				continue
+			}
+			colSt[i], digSt[i+u], uDigSt[i-u+n] = true, true, true
+			comb[u*n+i] = 'Q'
+			dfs(u + 1)
+			comb[u*n+i] = '.'
+			colSt[i], digSt[i+u], uDigSt[i-u+n] = false, false, false
+		}
+	}
+	dfs(0)
+	return res
+}
