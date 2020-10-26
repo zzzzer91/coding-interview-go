e690bae95a14ec8ed2ca9d217bb1a6f1383f8f81
zzzzer <zzzzer91@gmail.com>
2020-10-26 13:18:17 +0800

    feat: update

--- basis/data_structure/p011_数组模拟单链表/main.go
+++ basis/data_structure/p011_数组模拟单链表/main.go
@@ -4,40 +4,41 @@ import "fmt"
 
 const N = 1e5 + 10
 
-var tail, idx int // idx 表示当前用到了哪个节点，tail 存储链表尾
 var e, ne [N]int  // e 相当于链表的节点的 Val，ne 相当于 链表节点的 Next
+var head, idx int // idx 表示当前用到了哪个节点，head 存储链表头
 
 func main() {
 	initList()
-	insert(1)
-	insert(2)
-	insert(3)
-	remove()
+	insertToHead(1)
+	insertToHead(2)
+	insertToHead(3)
+	removeHead()
+	insertToHead(4)
 	traversal()
 }
 
 func initList() {
-	tail = -1 // 相当于 nil
+	head = -1 // 相当于 nil
 	idx = 0
 }
 
-// 插在链表尾
-func insert(a int) {
-	e[idx] = a
-	ne[idx] = tail
-	tail = idx
+// 插在链表头
+func insertToHead(x int) {
+	e[idx] = x
+	ne[idx] = head
+	head = idx
 	idx++
 }
 
-// 移除链表尾指向的节点
-func remove() {
-	if tail != -1 {
-		tail = ne[tail]
+// 删除头部
+func removeHead() {
+	if head != -1 {
+		head = ne[head]
 	}
 }
 
 func traversal() {
-	for i := 0; i <= tail; i++ {
+	for i := head; i != -1; i = ne[i] {
 		fmt.Println(e[i])
 	}
 }
