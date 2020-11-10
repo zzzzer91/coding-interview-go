05077ef4317b4901ce21f4d918b62497484ee439
zzzzer <zzzzer91@gmail.com>
2020-11-10 08:38:07 +0800

    feat: update

---
+++ leetcode/p0031_下一个排列/main.go
@@ -0,0 +1,38 @@
+// https://leetcode-cn.com/problems/next-permutation/
+
+package main
+
+func main() {
+
+}
+
+// 算法流程：
+// 1、从后向前查找第一个相邻升序的元素对 (i,j)，满足 A[i] < A[j]。此时 [j,end) 必然是降序
+// 2、在 [j,end) 从后向前查找第一个满足 A[i] < A[k] 的 k。A[i]、A[k] 分别就是上文所说的「小数」、「大数」
+// 3、将 A[i] 与 A[k] 交换
+// 4、可以断定这时 [j,end) 必然是降序，逆置 [j,end)，使其升序
+// 5、如果在步骤 1 找不到符合的相邻元素对，说明当前 [begin,end) 为一个降序顺序，则直接跳到步骤 4
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for j >= 0 && nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(a []int) {
+	l, r := 0, len(a)-1
+	for l < r {
+		a[l], a[r] = a[r], a[l]
+		l++
+		r--
+	}
+}
