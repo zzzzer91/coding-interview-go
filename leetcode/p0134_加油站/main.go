60c79f63f6533eb5482e0e04bede9b40fce13d15
zzzzer <zzzzer91@gmail.com>
2020-11-19 14:29:09 +0800

    feat: update

--- leetcode/p0134_加油站/main.go
+++ leetcode/p0134_加油站/main.go
@@ -14,7 +14,7 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	minIndex := 0
 	for i := 0; i < len(gas); i++ {
 		sum += gas[i] - cost[i]
-		if sum < min { // 找一个 sum 最小的地方作为起点
+		if sum < min {
 			min = sum
 			minIndex = i
 		}
@@ -22,5 +22,5 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	if sum < 0 {
 		return -1
 	}
-	return (minIndex + 1) % len(gas)
+	return (minIndex + 1) % len(gas) // 找一个 sum 最小的地方的下一个点作为起点
 }
--- leetcode/p0283_移动零/main.go
+++ leetcode/p0283_移动零/main.go
@@ -1,3 +1,5 @@
+// https://leetcode-cn.com/problems/move-zeroes/submissions/
+
 package main
 
 func main() {
@@ -6,9 +8,9 @@ func main() {
 
 func moveZeroes(nums []int) {
 	pos := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != 0 {
-			nums[pos] = nums[i]
+	for _, n := range nums {
+		if n != 0 {
+			nums[pos] = n
 			pos++
 		}
 	}
