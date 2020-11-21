041b0f41c4ecb26855f2ced1d279185205242803
zzzzer <zzzzer91@gmail.com>
2020-11-21 09:58:38 +0800

    feat: update

--- leetcode/p0148_排序链表/main.go
+++ leetcode/p0148_排序链表/main.go
@@ -54,3 +54,47 @@ func merge(p, q *ListNode) *ListNode {
 	}
 	return fakeHead.Next
 }
+
+// 迭代，空间复杂度为常数
+func sortList2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	length := 0
+	for node := head; node != nil; node = node.Next {
+		length++
+	}
+
+	dummyHead := &ListNode{Next: head}
+	for subLength := 1; subLength < length; subLength <<= 1 {
+		prev, cur := dummyHead, dummyHead.Next
+		for cur != nil {
+			head1 := cur
+			for i := 1; i < subLength && cur.Next != nil; i++ {
+				cur = cur.Next
+			}
+
+			head2 := cur.Next
+			cur.Next = nil
+			cur = head2
+			for i := 1; i < subLength && cur != nil && cur.Next != nil; i++ {
+				cur = cur.Next
+			}
+
+			var next *ListNode
+			if cur != nil {
+				next = cur.Next
+				cur.Next = nil
+			}
+
+			prev.Next = merge(head1, head2)
+
+			for prev.Next != nil {
+				prev = prev.Next
+			}
+			cur = next
+		}
+	}
+	return dummyHead.Next
+}
