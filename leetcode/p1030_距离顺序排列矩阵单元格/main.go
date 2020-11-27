f5c498f8bf68a29174c17d17c9e5c7603890ec49
zzzzer <zzzzer91@gmail.com>
2020-11-27 10:58:38 +0800

    feat: update

--- leetcode/p1030_距离顺序排列矩阵单元格/main.go
+++ leetcode/p1030_距离顺序排列矩阵单元格/main.go
@@ -7,22 +7,22 @@ func main() {
 }
 
 // 桶排序
-func allCellsDistOrder(n, m, r0, c0 int) [][]int {
-	maxDist := max(r0, n-1-r0) + max(c0, m-1-c0)
+func allCellsDistOrder(R, C, r0, c0 int) [][]int {
+	maxDist := max(r0, R-1-r0) + max(c0, C-1-c0)
 	buckets := make([][][]int, maxDist+1)
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
+	for i := 0; i < R; i++ {
+		for j := 0; j < C; j++ {
 			dist := abs(i-r0) + abs(j-c0)
 			buckets[dist] = append(buckets[dist], []int{i, j})
 		}
 	}
 
-	ans := make([][]int, 0, n*m)
+	res := make([][]int, 0, R*C)
 	for _, bucket := range buckets {
-		ans = append(ans, bucket...)
+		res = append(res, bucket...)
 	}
-	return ans
+	return res
 }
 
 func max(a, b int) int {
