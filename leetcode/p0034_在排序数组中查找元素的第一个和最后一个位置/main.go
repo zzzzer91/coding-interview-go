a931f78364d4caed53f066f5389153ad7fa7ed43
zzzzer <zzzzer91@gmail.com>
2020-12-01 18:34:30 +0800

    feat: update

--- leetcode/p0034_在排序数组中查找元素的第一个和最后一个位置/main.go
+++ leetcode/p0034_在排序数组中查找元素的第一个和最后一个位置/main.go
@@ -38,10 +38,10 @@ func findLastPos(nums []int, target int) int {
 	l, r := 0, len(nums)-1
 	for l < r {
 		m := l + (r-l+1)>>1
-		if nums[m] > target {
-			r = m - 1
-		} else {
+		if nums[m] <= target {
 			l = m
+		} else {
+			r = m - 1
 		}
 	}
 	if nums[l] != target {
