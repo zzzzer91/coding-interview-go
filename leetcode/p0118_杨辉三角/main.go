3b6076208d32b78b0a3fd5876bc6234089179455
zzzzer <zzzzer91@gmail.com>
2020-12-06 10:14:21 +0800

    feat: update

--- leetcode/p0118_杨辉三角/main.go
+++ leetcode/p0118_杨辉三角/main.go
@@ -1,3 +1,5 @@
+// https://leetcode-cn.com/problems/pascals-triangle/
+
 package main
 
 func main() {
@@ -5,15 +7,19 @@ func main() {
 }
 
 func generate(numRows int) [][]int {
-	var ret [][]int
-	for i := 0; i < numRows; i++ {
+	if numRows == 0 {
+		return nil
+	}
+	res := make([][]int, 0, numRows)
+	res = append(res, []int{1})
+	for i := 1; i < numRows; i++ {
+		preRow := res[i-1]
 		row := make([]int, i+1)
 		row[0], row[len(row)-1] = 1, 1
 		for j := 1; j < i; j++ {
-			preRow := ret[i-1]
 			row[j] = preRow[j-1] + preRow[j]
 		}
-		ret = append(ret, row)
+		res = append(res, row)
 	}
-	return ret
+	return res
 }
