ecbe37c3cf7222ecf13be6bf93287d2a05a03320
zzzzer <zzzzer91@gmail.com>
2021-05-05 13:28:42 +0800

    feat: update

--- leetcode/p0448_找到所有数组中消失的数字/main.go
+++ leetcode/p0448_找到所有数组中消失的数字/main.go
@@ -1,3 +1,5 @@
+// https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
+
 package main
 
 func main() {
@@ -7,8 +9,8 @@ func main() {
 // 将出现过的元素作为下标，将数组中对应元素标记为负的，
 // 若数组某元素为正的，说明其下标+1没在数组出现过，就是我们要找的值
 func findDisappearedNumbers(nums []int) []int {
-	for _, num := range nums {
-		idx := abs(num) - 1
+	for _, n := range nums {
+		idx := abs(n) - 1 // abs 不能少，因为可能已被其他的改成负的
 		nums[idx] = -abs(nums[idx])
 	}
 	var ret []int
@@ -25,4 +27,20 @@ func abs(x int) int {
 		return x
 	}
 	return -x
+}
+
+// 交换位置，第一种更好
+func findDisappearedNumbers2(nums []int) []int {
+	var res []int
+	for i := range nums {
+		for nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		}
+	}
+	for i, n := range nums {
+		if i != n-1 {
+			res = append(res, i+1)
+		}
+	}
+	return res
 }
