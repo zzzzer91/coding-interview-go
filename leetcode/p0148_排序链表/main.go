cb6fa5707b9ee70b585c061aa4b374e70bc80564
zzzzer <zzzzer91@gmail.com>
2021-05-06 09:32:38 +0800

    feat: update

--- leetcode/p0148_排序链表/main.go
+++ leetcode/p0148_排序链表/main.go
@@ -34,6 +34,47 @@ func findMidNode(head *ListNode) *ListNode {
 	return slowP
 }
 
+// 迭代，归并排序，空间复杂度为常数
+func sortList3(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	listLen := 0
+	for p := head; p != nil; p = p.Next {
+		listLen++
+	}
+	fakeHead := ListNode{Next: head}
+	for size := 1; size < listLen; size <<= 1 {
+		cur := fakeHead.Next // 注意这里不能用 head，head 排完序可能就不再头部了
+		tail := &fakeHead    // 用于将分割排序后的链表重新连接起来
+		for cur != nil {
+			left := cur                    // left(cur)->2->1->3->4->5...
+			right := cut(left, size)       // left(cur)->2 right->1->3->4->5...
+			cur = cut(right, size)         // left->2 right->1 cur->3->4->5->...
+			tail.Next = merge(left, right) // left(tail.Next)->1->2 cur->3->4->5->...
+			for tail.Next != nil {
+				tail = tail.Next
+			}
+		}
+	}
+	return fakeHead.Next
+}
+
+// 返回 head 跳过 size-1 个节点后的节点，并在节点前切断
+func cut(head *ListNode, size int) *ListNode {
+	p := head
+	for p != nil && size > 1 {
+		p = p.Next
+		size--
+	}
+	if p == nil {
+		return nil
+	}
+	next := p.Next
+	p.Next = nil
+	return next
+}
+
 func merge(p, q *ListNode) *ListNode {
 	fakeHead := ListNode{}
 	cur := &fakeHead
@@ -51,50 +92,6 @@ func merge(p, q *ListNode) *ListNode {
 		cur.Next = p
 	} else if q != nil {
 		cur.Next = q
-	}
-	return fakeHead.Next
-}
-
-// 迭代，空间复杂度为常数
-func sortList2(head *ListNode) *ListNode {
-	if head == nil || head.Next == nil {
-		return head
-	}
-
-	listLen := 0
-	for node := head; node != nil; node = node.Next {
-		listLen++
-	}
-
-	fakeHead := &ListNode{Next: head}
-	for subLen := 1; subLen < listLen; subLen <<= 1 {
-		prev, cur := fakeHead, fakeHead.Next
-		for cur != nil {
-			head1 := cur
-			for i := 1; i < subLen && cur.Next != nil; i++ {
-				cur = cur.Next
-			}
-
-			head2 := cur.Next
-			cur.Next = nil
-			cur = head2
-			for i := 1; i < subLen && cur != nil && cur.Next != nil; i++ {
-				cur = cur.Next
-			}
-
-			var next *ListNode
-			if cur != nil {
-				next = cur.Next
-				cur.Next = nil
-			}
-
-			prev.Next = merge(head1, head2)
-
-			for prev.Next != nil {
-				prev = prev.Next
-			}
-			cur = next
-		}
 	}
 	return fakeHead.Next
 }
